8b8792cb30d9c9443db60532b4f5c2020f2d17e0
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-06-12 20:47:38 +0530

    Modularized client side code

---
+++ Assets/Scripts/ClientUsernameDisplay_Lobby.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class ClientUsernameDisplay_Lobby : MonoBehaviour
+{
+    public TMP_Text _clientList;
+
+    async void Update()
+    {
+        await NetworkingClient.Instance.RequestClientList();
+    }
+
+    void Start()
+    {
+        NetworkingClient.OnClientJoin += AppendClientName;
+    }
+
+    void Oestroy()
+    {
+        NetworkingClient.OnClientJoin -= AppendClientName;
+    }
+    
+    private void AppendClientName(List<string> usernames)
+    {
+        _clientList.text = "";
+
+        foreach (var username in usernames)
+        {
+            _clientList.text += username + "\n";
+        }
+    }
+}
